5261c4dd8dba9b9a49d02cbc86bf6a9ef5139812
micmet <michalmetrycki@gmail.com>
2022-02-02 20:21:28 +0100

    once again

--- cypress/integration/actions.spec.js
+++ cypress/integration/actions.spec.js
@@ -41,7 +41,7 @@ context('Actions', () => {
     // https://on.cypress.io/focus
     cy.get('.action-focus').focus()
       .should('have.class', 'focus')
-      .prev().should('have.attr', 'style', 'color: blue;')
+      .prev().should('have.attr', 'style', 'color: orange;')
   })
 
   it('.blur() - blur off a DOM element', () => {
